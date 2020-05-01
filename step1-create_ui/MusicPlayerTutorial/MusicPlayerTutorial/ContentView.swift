5313b37a156e8e7700f727232e3f380f6c7f65fa
Keith Elliott <keith@gittie.com>
2020-05-01 11:24:00 -0400

    update starting files

---
+++ step1-create_ui/MusicPlayerTutorial/MusicPlayerTutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MusicPlayerTutorial
+//
+//  Created by Keith on 4/29/20.
+//  Copyright © 2020 GittieLabs. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
